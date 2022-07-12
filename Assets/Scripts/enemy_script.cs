42e8916a4da3f4716c9810e02c223cdbea93515e
Sepehr <62986692+IIIphr@users.noreply.github.com>
2022-07-12 10:08:59 +0430

    Added health numbers

--- Assets/Scripts/enemy_script.cs
+++ Assets/Scripts/enemy_script.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class enemy_script : MonoBehaviour
 {
     [SerializeField] Image health_bar;
+    [SerializeField] TextMeshProUGUI health_number;
     [SerializeField] float max_hp = 10;
     float hp = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        health_number.text = hp + "/" + max_hp;
     }
 
     public void die()
@@ -30,6 +32,7 @@ public class enemy_script : MonoBehaviour
             die();
         }
         health_bar.fillAmount = hp / max_hp;
+        health_number.text = hp + "/" + max_hp;
     }
 
     // Update is called once per frame
