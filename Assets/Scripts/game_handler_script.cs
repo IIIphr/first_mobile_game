fcd89a0150ad64825dd48221b79c935dd8c099d3
Sepehr <62986692+IIIphr@users.noreply.github.com>
2022-07-12 11:45:30 +0430

    Added player hp bar

--- Assets/Scripts/game_handler_script.cs
+++ Assets/Scripts/game_handler_script.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class game_handler_script : MonoBehaviour
 {
     [SerializeField] GameObject cards_container;
     [SerializeField] GameObject enemies_container;
+    [SerializeField] Image health_bar;
+    [SerializeField] TextMeshProUGUI health_number;
     [SerializeField] float player_max_hp = 10;
     float player_hp = 10;
     int difficulty = 1;
@@ -18,10 +22,17 @@ public class game_handler_script : MonoBehaviour
     void Start()
     {
         player_hp = player_max_hp;
+        set_health_text_and_bar();
         difficulty = PlayerPrefs.GetInt("difficulty", 1);
         cards_container.GetComponent<card_container_script>().fill_deck(5);
         enemies_container.GetComponent<enemy_cont_script>().set_diffculty(difficulty);
         enemies_container.GetComponent<enemy_cont_script>().start_spawning();
+    }
+
+    public void set_health_text_and_bar()
+    {
+        health_number.text = player_hp + "\n/\n" + player_max_hp;
+        health_bar.fillAmount = player_hp / player_max_hp;
     }
 
     public void load_main_menu()
@@ -41,6 +52,7 @@ public class game_handler_script : MonoBehaviour
         {
             SceneManager.LoadScene("game_over");
         }
+        set_health_text_and_bar();
     }
 
     // Update is called once per frame
